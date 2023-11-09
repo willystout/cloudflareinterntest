24e330890479c5f70009c2ab54d4321c9d686929
willystout <123514204+willystout@users.noreply.github.com>
2023-11-08 18:11:11 -0800

    added upgrades

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-const { parse } = require("csv-parse");
-const fs = require("fs");
-const result = [];
-fetch("./data")
-  // Retrieve its body as ReadableStream
-  .then((response) => response.body)
-  .then((body) => {
-    fs.createReadStream("general_data.csv")
-      .pipe(
-        parse({
-          comment: "#",
-          columns: true,
-        })
-      )
-      .on("data", (data) => {
-        console.log(data);
-      })
-      .on("error", (error) => {
-        console.log(error);
-      })
-      .on("end", () => {
-        console.log("Done");
-      });
+
+import { Readable } from "stream";
+const readableStream = await fetch(
+  'https://652c15ec.cloudflareinterntest.pages.dev/'
+  ).then((r) => Readable.fromWeb(r.body));
+readableStream
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    console.log(data);
+  })
+  .on("error", (error) => {
+    console.log(error);
+  })
+  .on("end", () => {
+    console.log("Done");
   });
+
+/*
+fs.createReadStream("general_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    console.log(data);
+  })
+  .on("error", (error) => {
+    console.log(error);
+  })
+  .on("end", () => {
+    console.log("Done");
+  });
+*/
