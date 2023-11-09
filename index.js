706aac9ec9f389e49e60ebfca4a485a9acf8d129
willystout <123514204+willystout@users.noreply.github.com>
2023-11-09 11:53:40 -0800

    fetch api

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-// 1) Fetch data from remote API
-async function getUsers() {
-  try {
-    const response = await fetch(event.request, {
-      method: "GET",
-    });
-
-    if (!response.ok) {
-      throw new Error(`Error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-const { parse } = require("csv-parse");
-const fs = require("fs");
-
-fs.createReadStream("general_data.csv")
+const requestUrl = "https://cloudflareinterntest.pages.dev/";
+fetch(requestUrl)
+.then(response => response.json())
+.then(data => { 
+   const { parse } = require("csv-parse");
+   const fs = require("fs");
+   fs.createReadStream("general_data.csv")
   .pipe(
     parse({
       comment: "#",
@@ -35,44 +20,4 @@ fs.createReadStream("general_data.csv")
   .on("end", () => {
     console.log("Done");
   });
-
-/*
-import { Readable } from "stream";
-const Response = await fetch(
-  'https://652c15ec.cloudflareinterntest.pages.dev/'
-  ).then((r) => Readable.fromWeb(r.body));
-Response
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    console.log(data);
-  })
-  .on("error", (error) => {
-    console.log(error);
-  })
-  .on("end", () => {
-    console.log("Done");
-  });
-
-fs.createReadStream("general_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    console.log(data);
-  })
-  .on("error", (error) => {
-    console.log(error);
-  })
-  .on("end", () => {
-    console.log("Done");
-  });
-
-*/
+})
