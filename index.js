28f74c884f7b09104eddc1ad9fe72fd836e4a750
willystout <123514204+willystout@users.noreply.github.com>
2023-11-08 18:00:46 -0800

    created it to respond to an api

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const {parse} = require('csv-parse');
-const fs = require('fs');
+const { parse } = require("csv-parse");
+const fs = require("fs");
 const result = [];
-
-fs.createReadStream('general_data.csv')
-.pipe(parse({
-  comment: '#',
-  columns: true,
-}))
-.on('data', (data) => {
-  console.log(data);
-}).on('error', (error) => {
-  console.log(error);
-}
-)
-.on('end', () => {
-  console.log('Done');
-})
+fetch("./tortoise.png")
+  // Retrieve its body as ReadableStream
+  .then((response) => response.body)
+  .then((body) => {
+    fs.createReadStream("general_data.csv")
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        console.log(data);
+      })
+      .on("error", (error) => {
+        console.log(error);
+      })
+      .on("end", () => {
+        console.log("Done");
+      });
+    // …
+  });
