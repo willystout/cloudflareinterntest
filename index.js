b93314d410856d76fe4832c41906a713749dddea
willystout <123514204+willystout@users.noreply.github.com>
2023-11-14 21:17:43 -0800

    updated hmtl

--- index.js
+++ index.js
@@ -1,84 +1,111 @@
-const csvtojson = require('csvtojson');
-const fs = require('fs');
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 
-// Input CSV file path
-const csvFilePath = 'general_data.csv';
+const ORG_NAMESPACE = new WorkersKV("d3cf2557154b4d8587724bfb800c5a23");
 
-// Output JSON file path
-const jsonFilePath = 'output.json';
+async function handleRequest(request) {
+  if (
+    request.method === "GET" &&
+    new URL(request.url).pathname === "/organization-chart"
+  ) {
+    try {
+      const orgData = await ORG_NAMESPACE.get("data", "json");
 
-// Read CSV file and convert to JSON
-csvtojson()
-  .fromFile(csvFilePath)
-  .then((jsonArray) => {
-    if (!Array.isArray(jsonArray)) {
-      console.error('Error: CSV data is not an array.');
-      return;
-    }
-
-    // Organize data into a hierarchical structure
-    const organizedData = organizeData(jsonArray);
-
-    // Write the organized data to a JSON file
-    fs.writeFile(jsonFilePath, JSON.stringify(organizedData, null, 2), (err) => {
-      if (err) {
-        console.error('Error writing JSON file:', err);
-      } else {
-        console.log('Conversion complete. JSON file created:', jsonFilePath);
+      if (!orgData) {
+        return new Response(
+          JSON.stringify({ error: "Organization data not found" }),
+          { status: 404 }
+        );
       }
+      const graphData = transformToGraph(orgData);
+
+      return new Response(JSON.stringify(graphData), {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      });
+    } catch (error) {
+      return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+        status: 500,
+      });
+    }
+  }
+  return new Response(JSON.stringify({ error: "Not Found" }), { status: 404 });
+}
+
+function transformToGraph(orgData) {
+  const csvtojson = require("csvtojson");
+  const fs = require("fs");
+  const csvFilePath = "general_data.csv";
+  const jsonFilePath = "output.json";
+
+  csvtojson()
+    .fromFile(csvFilePath)
+    .then((jsonArray) => {
+      if (!Array.isArray(jsonArray)) {
+        console.error("Error: CSV data is not an array.");
+        return;
+      }
+      const organizedData = organizeData(jsonArray);
+
+      fs.writeFile(
+        jsonFilePath,
+        JSON.stringify(organizedData, null, 2),
+        (err) => {
+          if (err) {
+            console.error("Error writing JSON file:", err);
+          } else {
+            console.log(
+              "Conversion complete. JSON file created:",
+              jsonFilePath
+            );
+          }
+        }
+      );
+    })
+    .catch((error) => {
+      console.error("Error converting CSV to JSON:", error);
     });
-  })
-  .catch((error) => {
-    console.error('Error converting CSV to JSON:', error);
-  });
 
-// Function to organize data into a hierarchical structure
-function organizeData(csvData) {
-  const organizedData = {};
+  function organizeData(csvData) {
+    const organizedData = {};
 
-  csvData.forEach((entry) => {
-    const organization = 'YourOrganization'; // Replace with your organization name
-    const departmentName = entry.department;
-    const employee = {
-      name: entry.name,
-      department: departmentName,
-      salary: parseFloat(entry.salary),
-      office: entry.office,
-      isManager: entry.isManager.toLowerCase() === 'true',
-      skills: [
-        entry.skill1.trim(),
-        entry.skill2.trim(),
-        entry.skill3.trim(),
-      ],
-    };
-
-    // Initialize organization if not exists
-    if (!organizedData[organization]) {
-      organizedData[organization] = { departments: [] };
-    }
-
-    // Find or create department
-    let department = organizedData[organization].departments.find(
-      (dep) => dep.DeptName === departmentName
-    );
-
-    if (!department) {
-      department = {
-        DeptName: departmentName,
-        managerName: '', // Default manager name
-        employees: [],
+    csvData.forEach((entry) => {
+      const organization = "YourOrganization";
+      const departmentName = entry.department;
+      const employee = {
+        name: entry.name,
+        department: departmentName,
+        salary: parseFloat(entry.salary),
+        office: entry.office,
+        isManager: entry.isManager.toLowerCase() === "true",
+        skills: [entry.skill1.trim(), entry.skill2.trim(), entry.skill3.trim()],
       };
-      organizedData[organization].departments.push(department);
-    }
 
-    // Checks if the employee is a manager
-    if (employee.isManager) {
-      department.managerName = employee.name;
-    }
+      if (!organizedData[organization]) {
+        organizedData[organization] = { departments: [] };
+      }
 
-    // Adds an employee to the department
-    department.employees.push(employee);
-  });
+      let department = organizedData[organization].departments.find(
+        (dep) => dep.DeptName === departmentName
+      );
 
-  return organizedData;
+      if (!department) {
+        department = {
+          DeptName: departmentName,
+          managerName: "",
+          employees: [],
+        };
+        organizedData[organization].departments.push(department);
+      }
+
+      if (employee.isManager) {
+        department.managerName = employee.name;
+      }
+
+      department.employees.push(employee);
+    });
+
+    return organizedData;
+  }
 }
